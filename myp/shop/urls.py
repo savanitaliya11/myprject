bb773815352c5842c4b5fa3d87aa0e31ddfc9953
savanitaliya11 <40891740+savanitaliya11@users.noreply.github.com>
2020-01-08 14:52:08 +0530

    connection successful

--- myp/shop/urls.py
+++ myp/shop/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('form', views.form, name='form'),
+    path('show', views.show, name='show'),
 ]
--- myp/shop/views.py
+++ myp/shop/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
 from django.http import HttpResponse
 
 
@@ -7,3 +8,19 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+
+def form(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        tel = request.POST.get('contact')
+        email = request.POST.get('email')
+        drop = request.POST.get('select')
+        mes = request.POST.get('message')
+        result = Students(name=name, contact=tel, email=email, select=drop, message=mes)
+        result.save()
+        return redirect('show')
+
+
+def show(request):
+    return HttpResponse('hey')
